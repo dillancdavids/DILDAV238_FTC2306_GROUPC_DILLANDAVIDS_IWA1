fe3f29197f64d7a1ee772f8e8cccb40922c36eba
dillancdavids <137268080+dillancdavids@users.noreply.github.com>
2023-08-02 11:16:58 +0200

    storing html elements in JavaScript

---
+++ scripts.js
@@ -0,0 +1,6 @@
+const number = document.querySelector('[data-key="number"]')
+const subtract = document.querySelector('[data-key="subract"]')
+const add = document.querySelector('[data-key="add"]')
+
+subtract.addEventListener
+add.addEventListener
